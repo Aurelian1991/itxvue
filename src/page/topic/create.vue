2cb4eb6f500d5f04159793207568758669f58ee3
chengwenliang <1015372312@qq.com>
2018-06-21 18:07:48 +0800

    JJ

--- src/page/topic/create.vue
+++ src/page/topic/create.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
-    <mu-appbar style="width: 100%; background-color:#263238;margin-bottom:1px;" title="记录中">
+    <mu-appbar style="width: 100%; background-color:#181928;margin-bottom:1px;" title="记录中">
       <mu-icon-button icon="close" slot="left" @click="goback" />
       <mu-icon-button icon="close" slot="right">
         <mu-icon value=":fa :fa-paper-plane " class="fa-paper-plane" aria-hidden="true" />
       </mu-icon-button>
     </mu-appbar>
-    <div class="content">
-      <mu-text-field v-model="content" multi-line :rows="6" full-width :underlineShow="false"></mu-text-field>
+    
+    <div class="content" >
+      <mu-text-field v-model="content" multi-line :rows="10" full-width :underlineShow="false" inputClass="textbox"></mu-text-field>
     </div>
+    <div class="add_button">
+         <mu-icon-button class="add">
+             <input type="file" class="file-button">
+            <mu-icon  value=":fa :fa-plus"  class="fa-plus"  aria-hidden="true">
+            </mu-icon>
+         </mu-icon-button>
+    </div>
+ 
   </div>
 </template>
 <style lang="less" scoped>
@@ -27,19 +36,18 @@
         this.$router.go(-1);
       },
       init() {
-        var that=this;
+        var that = this;
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(
             function (position) {
-              console.log()
               var longitude = position.coords.longitude;
               var latitude = position.coords.latitude;
-             var  url='http://api.map.baidu.com/geocoder/v2/?callback=renderReverse&location=31.206671,121.603762&output=json&pois=1&ak=lr7Cl7QkbGqH5irqlK5wZvwQ9duK84hc&coordtype=gcj02ll';
-              that.axios.get(url).then(response => {
-                console.log(response);
-            });
-              console.log(longitude)
-              console.log(latitude)
+                var longitude = 121.60378;
+              var latitude = 31.206696;
+                that.axios.get('position/gps',{params:{latitude:latitude,longitude:longitude}}).then((response) => {
+                  // console.log(response.data.data.formatted_address)
+              });
+
             },
             function (e) {
               var msg = e.code;
@@ -61,6 +69,19 @@
   };
 
 </script>
+<style lang="less">
+  .mu-appbar{
+      color: #e0e0e0;
+  }
+  .mu-text-field-input {
+    // color: #e0e0e0;
+        // color: #grey100;
+        color: #bdbdbd;
+        
+  }
+
+</style>
+
 <style lang="less" scoped>
   .container {
     position: absolute;
@@ -68,11 +89,35 @@
     width: 100%;
     width: 100%;
     height: 100%;
-    background-color: #212121;
+    background-color: #11121d;
+    color:#474a4f;
+
   }
 
   .content {
-    background-color: #757575;
+    background-color: #181928;
   }
+  .add_button{
+    border:  solid 1px;
+    overflow:hidden ;
+    display:inline-block; 
+    *display:inline; 
+    *zoom:1;
+    float:left;
+    margin:5px;
+  }
+  .add{
+    height: 100px;
+    width: 100px;
+  }
+.file-button{
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  opacity: 0;
+}
+
 
 </style>
